43de8d45dec52f2836fbf2284fb295f644142b4b
Angel Caceres Licona <angelcaceres@outlook.com>
2022-10-02 19:55:30 -0500

    Se modifican modelos

--- api_app/models.py
+++ api_app/models.py
@@ -18,9 +18,99 @@ ZODIAC_SIGNS = (
     ("Piscis", "Piscis"),
 )
 
+DRINKING_OPTIONS = (
+    ("Compañia", "Sólo en compañía"),
+    ("Nunca", "Nunca"),
+    ("Con frecuencia", "Con frecuencia"),
+    ("No bebo", "No bebo"),
+)
+
+INTEREST_OPTIONS = (
+    ("Creatividad", "Creatividad"),
+    ("Deportes", "Deportes"),
+    ("Salidas", "Salidas"),
+    ("Quedarse en casa", "Quedarse en casa"),
+    ("Cine y televisión", "Cine y televisión"),
+    ("Leer", "Leer"),
+    ("Música", "Música"),
+    ("Comida y bebida", "Comida y bebida"),
+    ("Viajes", "Viajes"),
+    ("Mascotas", "Mascotas"),
+    ("Valores y personalidad", "Valores y personalidad"),
+    ("Causas", "Causas"),
+)
+
+POLITICAL_VIEWS = (
+    ("Sin interés", "Sin interés"),
+    ("De centro", "De centro"),
+    ("Izquierda", "Izquierda"),
+    ("Derecha", "Derecha"),
+    ("Socialista", "Socialista"),
+)
+
+WORKOUT_OPTIONS = (
+    ("A menudo", "A menudo"),
+    ("A veces", "A veces"),
+    ("Casi nunca", "Casi nunca"),
+)
+
+EDUCATION_OPTIONS = (
+    ("Preparatoria", "Preparatoria"),
+    ("Formación profesional", "Formación profesional"),
+    ("Estudiando una licenciatura", "Estudiando una licenciatura"),
+    ("Bachillerato", "Bachillerato"),
+    ("Estudiando un posgrado", "Estudiando un posgrado"),
+    ("Posgrado", "Posgrado"),
+)
+
+SMOKING_OPTIONS = (
+    ("Sólo en compañía", "Sólo en compañía"),
+    ("Nunca", "Nunca"),
+    ("Habitualmente", "Habitualmente"),
+)
+
+GENDER_OPTIONS = (
+    ("Mujer", "Mujer"),
+    ("Hombre", "Hombre"),
+    ("No binario", "No binario"),
+    ("Otro", "Otro"),
+)
+
+KIDS_OPTIONS = (
+    ("Algún día", "Algún día"),
+    ("No quiero", "No quiero"),
+    ("Ya tengo y quiero mas", "Ya tengo y quiero mas"),
+    ("Ya tengo y no quiero mas", "Ya tengo y no quiero mas"),
+    ("Aún no se", "Aún no se"),
+)
+
+LOOKING_FOR_OPTIONS = (
+    ("Relación", "Relación"),
+    ("Nada serio", "Nada serio"),
+    ("No lo se aún", "No lo se aún"),
+    ("Matrimonio", "Matrimonio"),
+)
+
+RELIGION_OPTIONS = (
+    ("Agnosticismo", "Agnosticismo"),
+    ("Ateísmo", "Ateísmo"),
+    ("Budismo", "Budismo"),
+    ("Catolicismo", "Catolicismo"),
+    ("Cristianismo", "Cristianismo"),
+    ("Hinduismo", "Hinduismo"),
+    ("Judaismo", "Judaismo"),
+    ("Jainismo", "Jainismo"),
+    ("Mormonismo", "Mormonismo"),
+    ("Islam", "Islam"),
+    ("Zoroastrismo", "Zoroastrismo"),
+    ("Sijismo", "Sijismo"),
+    ("Espiritual", "Espiritual"),
+    ("Otra", "Otra"),
+)
+
+
 class InterestCategory(models.Model):
-    lang = models.CharField(max_length=2)
-    name = models.CharField(max_length=50)
+    lang = models.CharField(choices=INTEREST_OPTIONS, max_length=25)
 
 class Interest(models.Model):
     lang = models.CharField(max_length=2)
@@ -32,40 +122,32 @@ class ZodiacSign(models.Model):
     name = models.CharField(choices=ZODIAC_SIGNS, max_length=11)
 
 class PoliticalView(models.Model):
-    lang = models.CharField(max_length=2)
-    name = models.CharField(max_length=50)
+    name = models.CharField(choices=POLITICAL_VIEWS, max_length=11)
 
 class Workout(models.Model):
-    lang = models.CharField(max_length = 2)
-    name = models.CharField(max_length=50)
+    name = models.CharField(choices=WORKOUT_OPTIONS, max_length=10)
 
 class Education(models.Model):
-    lang = models.CharField(max_length = 2)
-    name = models.CharField(max_length=50)
+    name = models.CharField(choices=EDUCATION_OPTIONS, max_length=27)
 
 class Drinking(models.Model):
-    lang = models.CharField(max_length = 2)
-    name = models.CharField(max_length=50)
+    name = models.CharField(choices=DRINKING_OPTIONS, max_length=16)
 
 class Smoking(models.Model):
-    lang = models.CharField(max_length = 2)
-    name = models.CharField(max_length=50)
+    name = models.CharField(choices=SMOKING_OPTIONS, max_length=16)
 
 class Gender(models.Model):
-    lang = models.CharField(max_length = 2)
-    name = models.CharField(max_length=50)
+    show = models.BooleanField(default= True)
+    name = models.CharField(choices=GENDER_OPTIONS, max_length=10)
 
 class Kids(models.Model):
-    lang = models.CharField(max_length = 2)
-    name = models.CharField(max_length=50)
+    name = models.CharField(choices=KIDS_OPTIONS, max_length=25)
 
 class LookingFor(models.Model):
-    lang = models.CharField(max_length = 2)
-    name = models.CharField(max_length=50)
+    name = models.CharField(choices=LOOKING_FOR_OPTIONS, max_length=12)
 
 class Religion(models.Model):
-    lang = models.CharField(max_length = 2)
-    name = models.CharField(max_length=50)
+    name = models.CharField(choices=RELIGION_OPTIONS, max_length=12)
 
 class Languages(models.Model):
     lang = models.CharField(max_length = 2)
