55ec21780ae1a60beffdeb0f9bce79b8280a307a
Angel Caceres Licona <angelcaceres@outlook.com>
2022-10-16 21:11:37 -0500

    Updated serializer

--- api_app/serializers.py
+++ api_app/serializers.py
@@ -2,13 +2,6 @@ from rest_framework import serializers
 from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
-    first_name = serializers.CharField(max_length=200)
-    middle_name = serializers.CharField(max_length=200)
-    last_name = serializers.CharField(max_length=200)
-    bio = serializers.CharField(max_length=200)
-    date_of_birth = serializers.DateField()
-    
-
-class Meta:
-    model = User
-    fields = ('__all__')
+    class Meta:
+        model = User
+        fields = ('__all__')
